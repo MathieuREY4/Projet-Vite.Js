c06d8376feed747bbad650462604a8a017414a5c
Mathieu REY <mathieu.rey33@gmail.com>
2024-06-11 13:03:11 +0200

    Ajout de la PokémonCard de Bulbasaur dans la branch demandée

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+    return (
+        <>
+            <h1>Carte Pokémon</h1>
+            <figure>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image du Pokémon Bulbasaur" />
+                <figcaption>
+                    <p>Bulbasaur</p>
+                </figcaption>
+            </figure>
+        </>
+    );
+}
+
+export default PokemonCard;
