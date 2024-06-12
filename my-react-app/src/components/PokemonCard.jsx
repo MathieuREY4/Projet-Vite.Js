e641ee24e6f3ec3cf8c4632be8dfef96a8a3cfb2
Mathieu REY <mathieu.rey33@gmail.com>
2024-06-12 12:22:26 +0200

    Ajout des props

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,33 +1,23 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  
-  function PokemonCard() {
-      const pokemon = pokemonList[0];
-  
-      return (
-          <div>
-              <h1>Carte Pokémon</h1>
-              <figure>
-                  {pokemon.imgSrc ? (
-                      <img src={pokemon.imgSrc} alt={`Image du Pokémon ${pokemon.name}`} />
-                  ) : (
-                      <p>???</p>
-                  )}
-                  <figcaption>
-                      <p>{pokemon.name}</p>
-                  </figcaption>
-              </figure>
-          </div>
-      );
-  }
-  
-  export default PokemonCard;
-  
+/* eslint-disable react/prop-types */
+function PokemonCard({ pokemon }) {
+    console.log(pokemon);
+
+    return (
+        <div>
+            <h1>Carte Pokémon</h1>
+            <figure>
+                {pokemon.imgSrc ? (
+                    <img src={pokemon.imgSrc} alt={`Image du Pokémon ${pokemon.name}`} />
+                ) : (
+                    <p>???</p>
+                )}
+                <figcaption>
+                    <p>{pokemon.name}</p>
+                </figcaption>
+            </figure>
+        </div>
+    );
+}
+
+export default PokemonCard;
+
