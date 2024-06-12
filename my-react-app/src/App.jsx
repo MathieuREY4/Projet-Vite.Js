0ad1fcc91ef43cd9c33ef320a181b3ef39754978
Mathieu REY <mathieu.rey33@gmail.com>
2024-06-12 17:55:06 +0200

    Ajoute des states

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,30 +1,52 @@
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PokemonCard from './components/PokemonCard';
 
-function PokemonCard({ pokemon }) {
-    console.log(pokemon);
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "charmander",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+        name: "squirtle",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+        name: "pikachu",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+
+    const handleNext = () => {
+        if (pokemonIndex < pokemonList.length - 1) {
+            setPokemonIndex(pokemonIndex + 1);
+        }
+    };
+
+    const handlePrevious = () => {
+        if (pokemonIndex > 0) {
+            setPokemonIndex(pokemonIndex - 1);
+        }
+    };
 
     return (
-        <div>
-            <h1>Carte Pokémon</h1>
-            <figure>
-                {pokemon.imgSrc ? (
-                    <img src={pokemon.imgSrc} alt={`Image du Pokémon ${pokemon.name}`} />
-                ) : (
-                    <p>???</p>
-                )}
-                <figcaption>
-                    <p>{pokemon.name}</p>
-                </figcaption>
-            </figure>
+        <div className="App">
+            <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+            <div>
+                {pokemonIndex > 0 && <button onClick={handlePrevious}>Précédent</button>}
+                {pokemonIndex < pokemonList.length - 1 && <button onClick={handleNext}>Suivant</button>}
+            </div>
         </div>
     );
 }
 
-PokemonCard.propTypes = {
-    pokemon: PropTypes.shape({
-        imgSrc: PropTypes.string,
-        name: PropTypes.string.isRequired,
-    }).isRequired,
-};
-
-export default PokemonCard;
+export default App;
